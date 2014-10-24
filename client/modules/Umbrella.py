0e8562141d4e35f16e5b998a94aae2bf01603a16
mdecuir <mdecuir@gmail.com>
2014-10-24 11:46:01 -0700

    Make wit responses behave closer to the other stt engines.

--- client/modules/Umbrella.py
+++ client/modules/Umbrella.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8-*-
-#import datetime
+import datetime
 #from app_utils import getTimezone
 #from semantic.dates import DateService
 import forecastio
+import json
 
 WORDS = ["WEATHER", "TODAY", "TOMORROW"]
 
-#hardcoded latitude and longitude for Vulcan
+
 
 
 
@@ -23,6 +24,8 @@ def handle(text, mic, profile):
                    number)
     """
 
+    text_obj = json.loads(text)
+
     if not profile['location']:
         mic.say(
             "I don't know where you are.")
@@ -36,11 +39,27 @@ def handle(text, mic, profile):
     api_key = profile['keys']['forecastio']
     lat = profile['location']['latitude']
     lng = profile['location']['longitude']
+    now = datetime.datetime.now() #this should be determined dynamically
 
-    forecast = forecastio.load_forecast(api_key, lat, lng)
+    forecast = forecastio.load_forecast(api_key, lat, lng, now)
 
     hourly = forecast.hourly()
+
+    umbrella_needed = False
+    for datum in hourly.data:
+        timewindow = datum.time().time()
+        #we should be subsetting this down based on time based config
+        precipProbability = datum.d[u'precipProbability']
+        precipIntensity = datum.d[u'precipIntensity']
+        precipType = datum.d.get(u'precipType')
+        if precipType == u'rain' and precipProbability >= .5 and precipIntensity >= .01:
+            umbrella_needed = True
+
     output = hourly.summary
+    if umbrella_needed:
+        output += u' You should bring an umbrella today.'
+    else:
+        output += u' You do not need an umbrella today.'
 
     mic.say(output)
 
@@ -52,4 +71,5 @@ def isValid(text):
         Arguments:
         text -- user-input, typically transcribed speech
     """
-    return any(d[u'intent'] == u'need_umbrella' for d in text[u'outcomes'])
+    text_obj = json.loads(text)
+    return any(d[u'intent'] == u'need_umbrella' for d in text_obj[u'outcomes'])
